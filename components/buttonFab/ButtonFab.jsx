1b003f97033dc97592c2318c81dd1d0433ea0362
Fabian Arevalo Roa <arevalo.cpweb@gmail.com>
2022-11-22 09:27:20 +0000

    Feature(ButtonFab): Se creo la estructura del componente)

--- components/buttonFab/ButtonFab.jsx
+++ components/buttonFab/ButtonFab.jsx
@@ -1,14 +1,37 @@
-import { StyleSheet, View} from 'react-native';
+import { Pressable, StyleSheet, Text } from 'react-native';
 
 const ButtonFab = () => {
 
     return (
-        <View>
-        </View>  
+        <Pressable style={({ pressed }) => [
+            {
+                backgroundColor: pressed
+                ? '#BBD2F2'
+                : '#5694EF'
+            },
+            styles.pressableStyle
+        ]}>
+            <Text style={styles.newMovement}>+</Text>
+        </Pressable>
     )
 }
 
 const styles = StyleSheet.create({
+
+    pressableStyle: {
+        borderRadius: 50,
+        position: 'absolute',
+        right: 50,
+        bottom: 50
+    },
+
+    newMovement: {
+        width: 50,
+        height: 50,
+        textAlign: 'center',
+        borderRadius: 50,
+        fontSize: 35
+    },
 
 });
 
