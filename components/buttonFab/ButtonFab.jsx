b29b974d7bee30334d3862e4f3ed230a3d9f7a57
Fabian Arevalo Roa <arevalo.cpweb@gmail.com>
2022-11-22 09:16:59 +0000

    Feature(ButtonFab): Se creo la carpeta(components) y el componete(ButtonFab)

---
+++ components/buttonFab/ButtonFab.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, View} from 'react-native';
+
+const ButtonFab = () => {
+
+    return (
+        <View>
+        </View>  
+    )
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default ButtonFab
